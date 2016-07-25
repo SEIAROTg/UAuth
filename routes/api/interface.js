f202f00f11fe0d77a6fb5336e8129f2ad3abb363
SEIAROTg <seiarotg@gmail.com>
2016-07-25 22:03:13 +0800

    change api

--- routes/api/interface.js
+++ routes/api/interface.js
@@ -4,35 +4,54 @@ const express = require('express');
 const router = express.Router();
 const assert = require('chai').assert;
 
-router.post('/interface', (req, res) => {
+
+// 列出app
+router.get('/all', (req, res) => {
 
 });
 
-router.get('/interface/:id', (req, res) => {
+// 列出属于当前用户的app
+router.get('/my', (req, res) => {
 
 });
 
-router.post('/interface/:id/approve', (req, res) => {
+// 申请接口
+router.post('/', (req, res) => {
 
 });
 
-router.post('/interface/:id/reject', (req, res) => {
+// 查看app信息
+router.get('/:id', (req, res) => {
 
 });
 
-router.post('/interface/:id/revoke', (req, res) => {
+// 批准接口申请
+router.post('/:id/approve', (req, res) => {
 
 });
 
-router.post('/interface/:id/transfer', (req, res) => {
+// 拒绝接口申请
+router.post('/:id/reject', (req, res) => {
 
 });
 
-router.post('/interface/:id/reset', (req, res) => {
+// 吊销接口
+router.post('/:id/revoke', (req, res) => {
 
 });
 
-router.put('/interface/:id', (req, res) => {
+// 转让app
+router.post('/:id/transfer', (req, res) => {
+
+});
+
+// 重置appsecret
+router.post('/:id/reset', (req, res) => {
+
+});
+
+// 修改app
+router.patch('/:id', (req, res) => {
 
 });
 
