d86b55fb263a48a9b1349548643b16f3013122a7
SEIAROTg <seiarotg@gmail.com>
2016-07-26 18:49:40 +0800

    change assertion to ensure message to be output

--- routes/oauth.js
+++ routes/oauth.js
@@ -8,14 +8,14 @@ const login = require('./login');
 
 router.all('/authorize', login, (req, res) => {
 	assert.strictEqual(req.query.response_type, 'code', 'response_type must be `code`');
-	assert.lengthOf(req.query.appid, 18, 'invalid appid');
+	assert(typeof req.query.appid === 'string' && req.query.appid.length === 18, 'invalid appid');
 	assert.oneOf(req.query.scope, [], 'invalid scope');
 });
 
 router.get('/userinfo', (req, res) => {
 	assert.strictEqual(req.query.grant_type, 'authorization_code', 'grant_type must be `authorization_code`');
-	assert.lengthOf(req.query.appid, 18, 'invalid appid');
-	assert.lengthOf(req.query.appsecret, 32, 'invalid appsecret');
+	assert(typeof req.query.appid === 'string' && req.query.appid.length === 18, 'invalid appid');
+	assert(typeof req.query.appsecret === 'string' && req.query.appsecret.length === 32, 'invalid appsecret');
 });
 
 module.exports = router;
