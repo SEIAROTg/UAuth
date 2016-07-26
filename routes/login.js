3173c639655febd71f2f16cf60cf9ffda88b43f4
SEIAROTg <seiarotg@gmail.com>
2016-07-26 18:46:29 +0800

    remove debugging info

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,9 @@
 "use strict";
 
 module.exports = (req, res, next) => {
-	console.log(req.session);
 	if (req.body.isLogin !== undefined) {
 		// TODO: preform login
 		req.session.userId = 6500001;
-		console.log(req.session);
 	}
 	if (req.session.userId) {
 		next();
