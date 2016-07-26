99312ba0840882fdf57fceb5a60ee9b591f4e999
SEIAROTg <seiarotg@gmail.com>
2016-07-26 18:46:51 +0800

    make AssertionError return HTTP 400

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ module.exports = () => {
 	// will print stacktrace
 	if (app.get('env') === 'development') {
 		app.use((err, req, res, next) => {
-			res.status(err.status || 500);
+			res.status(err.status || (err.name === 'AssertionError' ? 400 : 500));
 			res.render('error', {
 				message: err.message,
 				error: err
@@ -60,7 +60,7 @@ module.exports = () => {
 	// production error handler
 	// no stacktraces leaked to user
 	app.use((err, req, res, next) => {
-		res.status(err.status || 500);
+		res.status(err.status || (err.name === 'AssertionError' ? 400 : 500));
 		res.render('error', {
 			message: err.message,
 			error: {}
