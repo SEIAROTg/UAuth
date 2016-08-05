a674e0c2f62701f34f0be613597d8c0707c2cd4b
SEIAROTg <seiarotg@gmail.com>
2016-08-05 09:54:03 +0800

    add setup script

---
+++ setup.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+const co = require('co');
+const mariasql = require('mariasql');
+const promisify = require('./common').promisify;
+
+co(function*() {
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.question = promisify(rl.question, true);
+
+yield rl.question(`Before continuing, please first:
+1. Create a database for UAuth
+2. Configure appropriate permission for the created database
+3. Enable global scheduler by running "SET GLOBAL event_scheduler = ON;"
+
+Press enter to continue.
+`);
+
+let host = (yield rl.question('Database Host (default to localhost):')) || 'localhost';
+let port = parseInt(yield rl.question('Database Port (default to 3306):')) || 3306;
+let dbName = (yield rl.question('Database Name (default to UAuth):')) || 'UAuth';
+let user = yield rl.question('Database Username:');
+let password = yield rl.question('Database Password:');
+
+rl.close();
+
+// Create database
+
+let db = new mariasql();
+db.connect = promisify(db.connect);
+db.query = promisify(db.query);
+
+yield db.connect({
+	host,
+	port,
+	user,
+	password,
+	db: dbName,
+});
+
+yield db.query(`CREATE TABLE IF NOT EXISTS app (
+	appid VARCHAR(18) PRIMARY KEY,
+	appsecret VARCHAR(40) NOT NULL,
+	name TEXT NOT NULL,
+	ownerType ENUM("individual", "org", "university") NOT NULL,
+	ownerName TEXT NOT NULL,
+	ownerUser VARCHAR(7) NOT NULL,
+	description TEXT NOT NULL,
+	status ENUM("pending", "rejected", "enabled", "disabled", "deleted") NOT NULL,
+	scope TEXT NOT NULL,
+	reason TEXT NOT NULL,
+	domains TEXT NOT NULL
+)`);
+
+yield db.query(`CREATE TABLE IF NOT EXISTS code (
+	code VARCHAR(16) PRIMARY KEY,
+	appid VARCHAR(18) NOT NULL,
+	user VARCHAR(7) NOT NULL,
+	scope TEXT NOT NULL,
+	expiration DATETIME NOT NULL,
+	FOREIGN KEY (appid) REFERENCES app(appid)
+)`);
+
+yield db.query(`CREATE TABLE IF NOT EXISTS admin (
+	userId VARCHAR(7) NOT NULL,
+	isRoot BOOLEAN NOT NULL
+)`);
+
+yield db.query(`CREATE EVENT IF NOT EXISTS clearExpiredCode
+ON SCHEDULE
+	EVERY 1 DAY
+DO
+	BEGIN
+		DELETE FROM code WHERE expiration < NOW();
+	END
+`);
+
+db.close();
+
+let config = JSON.parse(yield promisify(fs.readFile)(path.resolve(__dirname, 'config.json')));
+Object.assign(config, {
+	db: {
+		host,
+		port,
+		user,
+		password,
+		db: dbName,
+	}
+});
+yield promisify(fs.writeFile)(path.resolve(__dirname, 'config.json'), JSON.stringify(config, null, '\t'));
+
+}).done();
