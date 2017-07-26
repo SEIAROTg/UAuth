72a332e522b329d1ee128edb5b03176864eb1871
SEIAROTg <seiarotg@gmail.com>
2017-07-26 23:13:47 +0800

    add routes

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	dashboard : require('./dashboard'),
+	wechat    : require('./wechat'),
+	login     : require('./login'),
+	logout    : require('./logout'),
+	authorize : require('./authorize'),
+	userInfo  : require('./user-info'),
+};
